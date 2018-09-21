faa7b8c26994d87e52a00aece7487d5690851a74
workerMan <drwang@gmail.com>
2018-09-21 17:41:18 +0800

    更新

---
+++ application/api/controller/User.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * fileName: User.php
+ * User: zhihuanwang
+ * Date: 2018/9/19
+ * Time: 14:28
+ */
+
+namespace app\api\controller;
+use think\Controller;
+
+class User extends Controller
+{
+    /********************************************
+     * @purpose  普通用户注册
+     * @date 2018/9/19 14:30
+     * @param 
+     * @return 
+     *******************************************/
+    public function doRegister()
+    {
+        
+        
+    }
+    /********************************************
+     * @purpose  普通用户登录
+     * @date 2018/9/19 14:31
+     * @param 
+     * @return 
+     *******************************************/
+    public function doLogin()
+    {
+        
+    }
+    /********************************************
+     * @purpose  普通用户登出
+     * @date 2018/9/19 14:31
+     * @param 
+     * @return 
+     *******************************************/
+    public function doLogout()
+    {
+        
+    }
+    /********************************************
+     * @purpose  
+     * @date 2018/9/19 14:32
+     * @param 
+     * @return 
+     *******************************************/
+
+}
